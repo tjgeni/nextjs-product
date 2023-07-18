fb9fd90ad1ef468fd7c3d342a47396436adfeb24
tegarjgap <tegarjagatgeni@gmail.com>
2023-07-18 21:26:46 +0700

    fix: remove log

--- src/app/products/services/index.ts
+++ src/app/products/services/index.ts
@@ -1,9 +1,7 @@
 import { getBaseUrl } from "@/lib/getBaseURL";
 
 export async function getAllProduct() {
-  console.log(getBaseUrl());
   const response = await fetch(`${getBaseUrl()}/api/products`);
-  console.log(response);
   if (!response.ok) {
     throw new Error("Failed to fetch all products");
   }
