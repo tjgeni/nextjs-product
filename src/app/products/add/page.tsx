cc0b8a92b71669aecb0c30f3ea27430257335aa1
tegarjgap <tegarjagatgeni@gmail.com>
2023-07-18 20:34:17 +0700

    feat: remove unused route

--- src/app/products/add/page.tsx
+++ src/app/products/add/page.tsx
@@ -4,9 +4,6 @@ import Link from "next/link";
 export default function Add() {
   return (
     <>
-      <Link href={"/products"}>
-        <p className="p-5 underline">👈 Back to List of Product</p>
-      </Link>
       <div className="max-w-screen-sm m-auto">
         <FormUI />
       </div>
