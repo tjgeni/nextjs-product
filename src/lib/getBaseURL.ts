3961cfb7f1ae15d4f5661e2e765b05d0a83cc638
tegarjgap <tegarjagatgeni@gmail.com>
2023-07-18 21:15:28 +0700

    fix: base url last part

--- src/lib/getBaseURL.ts
+++ src/lib/getBaseURL.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 
 export const getBaseUrl = cache(() =>
-  process.env.URL_STRING
-    ? `https://nextjs-mantap-mania.vercel.app/`
+  process.env.VERCEL_URL
+    ? `https://nextjs-mantap-mania.vercel.app`
     : `http://localhost:${process.env.PORT ?? 3000}`
 );
