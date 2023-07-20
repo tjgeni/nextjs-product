0adf005c22c9345705c6280e118dd6ec45356e35
tegarjgap <tegarjagatgeni@gmail.com>
2023-07-20 09:55:44 +0700

    fix: add revalidate part 2

--- src/app/products/services/getAllProduct.ts
+++ src/app/products/services/getAllProduct.ts
@@ -2,7 +2,7 @@ import { getBaseUrl } from "@/lib/getBaseURL";
 
 export async function getAllProduct() {
   const response = await fetch(`${getBaseUrl()}/api/products/get-all`, {
-    next: { revalidate: 0 },
+    next: { revalidate: 2 },
   });
   if (!response.ok) throw new Error("Failed to fetch all products");
 
