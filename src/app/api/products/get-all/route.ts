6db343dbb3b6577f6f490a4552ad44f3a786af53
Tim Neutkens <tim@timneutkens.nl>
2023-08-01 09:12:22 +0200

    Make sure api route is not static.

--- src/app/api/products/get-all/route.ts
+++ src/app/api/products/get-all/route.ts
@@ -2,6 +2,8 @@ import { db } from "@/db/drizzle-client";
 import { products } from "@/db/schema";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   const allProduct = await db.select().from(products);
   return NextResponse.json(allProduct);
